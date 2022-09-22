1e996b059bc06a46bcdf623cf790e40bcd6995a1
Daniel <danny.melchior28041999@gmail.com>
2022-09-22 18:02:05 +0200

    Neue Database und neue Tabellen
    !Bitte in Zukunft aus dem SQL-Script_TestFILE die Query auf der Datenbank ausführen!

--- src/main/java/com/example/kinobackend/db_access/MySqlConnector.java
+++ src/main/java/com/example/kinobackend/db_access/MySqlConnector.java
@@ -11,7 +11,7 @@ public class MySqlConnector {
     public MySqlConnector(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinedb", "root", "DBADMIN");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "DBADMIN");
         }catch (Exception e){
             System.out.println(e);
         }
