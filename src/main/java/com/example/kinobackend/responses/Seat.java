dbe0f618e5a92666a5178f1fcd228c494d8e3143
Murat <Murat>
2022-09-24 23:13:08 +0200

    Class for Seats

---
+++ src/main/java/com/example/kinobackend/responses/Seat.java
@@ -0,0 +1,62 @@
+package com.example.kinobackend.responses;
+import java.math.BigDecimal;
+public class Seat {
+
+    private int seatId;
+
+    private boolean isFree;
+
+    private BigDecimal Price;
+
+    private char row;
+
+    private int number;
+
+    public Seat(int seatId, boolean isFree, BigDecimal price, char row, int number) {
+        this.seatId = seatId;
+        this.isFree = isFree;
+        Price = price;
+        this.row = row;
+        this.number = number;
+    }
+
+    public int getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(int seatId) {
+        this.seatId = seatId;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public void setFree(boolean free) {
+        isFree = free;
+    }
+
+    public BigDecimal getPrice() {
+        return Price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        Price = price;
+    }
+
+    public char getRow() {
+        return row;
+    }
+
+    public void setRow(char row) {
+        this.row = row;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
