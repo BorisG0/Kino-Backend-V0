ab2871cfc939f65a8eabc82e52dc561dc83f74a9
Boris Gratchev <boris@gratchev.com>
2022-10-06 14:09:39 +0200

    seats in event

--- src/main/java/com/example/kinobackend/controllers/SeatController.java
+++ src/main/java/com/example/kinobackend/controllers/SeatController.java
@@ -1,8 +1,12 @@
 package com.example.kinobackend.controllers;
 
 import com.example.kinobackend.db_access.SeatSQL;
+import com.example.kinobackend.responses.Event;
 import com.example.kinobackend.responses.Seat;
+import com.example.kinobackend.responses.SeatInEvent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +17,20 @@ public class SeatController {
         SeatSQL connector = new SeatSQL();
         Seat[] Seats = connector.getSeatData();
         return Seats;
+    }
+
+    @PostMapping("/api/seatsInEvent")
+    public SeatInEvent[] getSeatsInEventId(@RequestBody int eventId){
+        System.out.println("getting seats for eventId: " + eventId);
+        SeatSQL connector = new SeatSQL();
+        //SeatInEvent[] seats = connector.getSeatsForEvent(new Event(1,null,null,1,1));
+        SeatInEvent[] seats = connector.getSeatsForEventId(eventId);
+
+        for(SeatInEvent s: seats){
+            System.out.println(s.getRow() + s.getNumberInRow() + s.getStatus());
+        }
+
+        return seats;
     }
 
 }
--- src/main/java/com/example/kinobackend/db_access/SeatSQL.java
+++ src/main/java/com/example/kinobackend/db_access/SeatSQL.java
@@ -1,6 +1,8 @@
 package com.example.kinobackend.db_access;
 
+import com.example.kinobackend.responses.Event;
 import com.example.kinobackend.responses.Seat;
+import com.example.kinobackend.responses.SeatInEvent;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -23,6 +25,26 @@ public final class SeatSQL extends MySqlConnector {
         }
 
         return data.toArray(new Seat[0]);
+    }
+
+    public SeatInEvent[] getSeatsForEventId(int eventId){
+        ArrayList<SeatInEvent> data = new ArrayList<>();
+
+        try{
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select s.Line, s.NumberInLine, sie.status " +
+                    "from seats_in_event sie, seats s where sie.seat_id = s.idSeat and event_id = "
+                    + eventId);
+
+            while(rs.next()){
+                data.add(new SeatInEvent(rs.getString(1), rs.getInt(2), rs.getInt(3)));
+            }
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        return data.toArray(new SeatInEvent[0]);
     }
 
 }
