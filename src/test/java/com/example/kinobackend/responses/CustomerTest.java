2a4762f08d24e91667f782eea1ce1d391b9445be
Lorena <lorena.hanser@yahoo.com>
2022-10-06 14:21:03 +0200

    Added new attributes in CustomerTest

--- src/test/java/com/example/kinobackend/responses/CustomerTest.java
+++ src/test/java/com/example/kinobackend/responses/CustomerTest.java
@@ -8,22 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
 
-    String mailAdress = "mailAdress";
+    String mailAdress = "VornameNachname@mailadresse.de";
     String lastName = "Nachname";
     String firstName = "Vorname";
     int age = 18;
     int postalCode = 12345;
     int houseNumber = 1;
-    String location = "location";
-    String street = "street";
-    String country = "country";
-    String mobileNumber = "123";
-    String password = "password";
+    String location = "Mannheim";
+    String street = "Hauptstraße";
+    String countryCode = "DE";
+    String mobileNumber = "0049123456789";
+    String password = "sicheresPasswort";
     Customer testCustomer;
 
     @BeforeEach
     void setUp(){
-        testCustomer = new Customer(mailAdress, lastName, firstName, age,postalCode,houseNumber,location,street,country,mobileNumber,password);
+        testCustomer = new Customer(mailAdress, lastName, firstName, age,postalCode,houseNumber,location,street,countryCode,mobileNumber,password);
     }
 
     @AfterEach
@@ -68,6 +68,101 @@ class CustomerTest {
     }
 
     @Test
+    void getPostalCode() {
+        assertEquals(postalCode, testCustomer.getPostalCode());
+    }
+
+    @Test
+    void setPostalCode() {
+        int setPostalCode = 98765;
+        testCustomer.setPostalCode(setPostalCode);
+        assertEquals(setPostalCode, testCustomer.getPostalCode());
+    }
+
+    @Test
+    void getHouseNummer() {
+        assertEquals(houseNumber, testCustomer.getHouseNumber());
+    }
+
+    @Test
+    void setHouseNumber() {
+        int setHouseNumber = 76;
+        testCustomer.setHouseNumber(setHouseNumber);
+        assertEquals(setHouseNumber, testCustomer.getHouseNumber());
+    }
+
+    @Test
+    void getLocation() {
+        assertEquals(location, testCustomer.getLocation());
+    }
+
+    @Test
+    void setLocation() {
+        String setLocation = "Hamburg";
+        testCustomer.setLocation(setLocation);
+        assertEquals(setLocation, testCustomer.getLocation());
+    }
+
+    @Test
+    void getStreet() {
+        assertEquals(street, testCustomer.getStreet());
+    }
+
+    @Test
+    void setStreet() {
+        String setStreet = "Bahnhofstraße";
+        testCustomer.setStreet(setStreet);
+        assertEquals(setStreet, testCustomer.getStreet());
+    }
+
+    @Test
+    void getCountryCode() {
+        assertEquals(countryCode, testCustomer.getCountryCode());
+    }
+
+    @Test
+    void setCountryCode() {
+        String setCountryCode = "FR";
+        testCustomer.setCountryCode(setCountryCode);
+        assertEquals(setCountryCode, testCustomer.getCountryCode());
+    }
+
+    @Test
+    void getMobileNumber() {
+        assertEquals(mobileNumber, testCustomer.getMobileNumber());
+    }
+
+    @Test
+    void setMobileNumber() {
+        String setMobileNumber = "+331234567";
+        testCustomer.setMobileNumber(setMobileNumber);
+        assertEquals(setMobileNumber, testCustomer.getMobileNumber());
+    }
+
+    @Test
+    void getPassword() {
+        assertEquals(password, testCustomer.getPassword());
+    }
+
+    @Test
+    void setPassword() {
+        String setPassword = "geheim!!!";
+        testCustomer.setPassword(setPassword);
+        assertEquals(setPassword, testCustomer.getPassword());
+    }
+
+    @Test
     void testConstructor(){
+        assertEquals(mailAdress, testCustomer.getMailAdress());
+        assertEquals(lastName, testCustomer.getLastname());
+        assertEquals(firstName, testCustomer.getFirstname());
+        assertEquals(age, testCustomer.getAge());
+        assertEquals(postalCode, testCustomer.getPostalCode());
+        assertEquals(houseNumber, testCustomer.getHouseNumber());
+        assertEquals(location, testCustomer.getLocation());
+        assertEquals(street, testCustomer.getStreet());
+        assertEquals(countryCode, testCustomer.getCountryCode());
+        assertEquals(mobileNumber, testCustomer.getMobileNumber());
+        assertEquals(password, testCustomer.getPassword());
     }
 }
