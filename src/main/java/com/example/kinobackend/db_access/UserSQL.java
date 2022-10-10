582712796b81c83bcd64c545f8a10a10aaf99c4b
Lorena <lorena.hanser@yahoo.com>
2022-10-10 22:26:26 +0200

    Updated UserSQLTest cause tests failed after changes in UserSQL

--- src/main/java/com/example/kinobackend/db_access/UserSQL.java
+++ src/main/java/com/example/kinobackend/db_access/UserSQL.java
@@ -17,12 +17,12 @@ public class UserSQL extends MySqlConnector {
                 return "Employee";
             }else{
                 rs = stmt.executeQuery("select * from customer where MailAddress = " + putStringIntoApostrophe(mailAdress) + " and Password = " + putStringIntoApostrophe(password));
-            if (rs.next() == true){
-                //return User = new Customer(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getInt(6), rs.getString(7), rs.getString(8), rs.getString(9),rs.getString(10), rs.getString(11));
-                return "Customer";
+                if (rs.next() == true){
+                    //return User = new Customer(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getInt(6), rs.getString(7), rs.getString(8), rs.getString(9),rs.getString(10), rs.getString(11));
+                    return "Customer";
+                }
             }
-            }
-              return "NoUserFound";
+            return "NoUserFound";
         } catch (Exception e) {
             System.out.println(e);
             return "Error Accessing Data";
