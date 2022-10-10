96ae8067dd4e7c88743c65ca91e6db573034b934
Mel Wieczorek <mel.wieczorek@gmx.de>
2022-10-10 11:28:19 +0200

    Added addEmployee and getEmployeeByMailAdress

--- src/main/java/com/example/kinobackend/db_access/EmployeeSQL.java
+++ src/main/java/com/example/kinobackend/db_access/EmployeeSQL.java
@@ -1,7 +1,6 @@
 package com.example.kinobackend.db_access;
 
 import com.example.kinobackend.responses.Employee;
-
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -25,6 +24,26 @@ public class EmployeeSQL extends MySqlConnector{
 
         return data.toArray(new Employee[0]);
 
+    }
+    public Employee getEmployeeByMailAdress(String mailAdress){
+        Employee employee = null;
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from employee where MailAdress = "+putStringIntoApostrophe(mailAdress));
+            rs.next();
+            employee = new Employee(rs.getString(1),rs.getString(2), rs.getString(3),rs.getString(4), rs.getString(5));
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return employee;
+    }
+    public void addEmployee(Employee employee){
+        try {
+            Statement stmt = con.createStatement();
+            stmt.execute("insert into employee (MailAddress, LastName, FirstName, Role, Password ) values (" + putStringIntoApostrophe(employee.getMailAdress())+", "+putStringIntoApostrophe(employee.getLastname())+", "+putStringIntoApostrophe(employee.getFirstname())+", "+putStringIntoApostrophe(employee.getRole())+", "+putStringIntoApostrophe(employee.getPassword()));
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 
 }
