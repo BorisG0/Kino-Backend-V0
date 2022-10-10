1203ad9df2cc9c867b6ca096d42ea2643e91d00a
Lorena <lorena.hanser@yahoo.com>
2022-10-10 16:17:04 +0200

    Updated name of db column (MailAddress)

--- src/main/java/com/example/kinobackend/db_access/CustomerSQL.java
+++ src/main/java/com/example/kinobackend/db_access/CustomerSQL.java
@@ -28,7 +28,7 @@ public final class CustomerSQL extends MySqlConnector{
         Customer customer = null;
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from customer where MailAdress = "+putStringIntoApostrophe(mailAdress));
+            ResultSet rs = stmt.executeQuery("select * from customer where MailAddress = "+putStringIntoApostrophe(mailAdress));
             rs.next();
             customer = new Customer(rs.getString(1),rs.getString(2),rs.getString(3),rs.getInt(4), rs.getInt(5),rs.getInt(6),rs.getString(7), rs.getString(8), rs.getString(9),rs.getString(10),rs.getString(11));
         }catch (Exception e){
