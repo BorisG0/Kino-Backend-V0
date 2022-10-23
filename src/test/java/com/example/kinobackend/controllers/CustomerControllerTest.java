dcd6139a5524db98af714de24b2035b3dc719c6b
Lorena <lorena.hanser@yahoo.com>
2022-10-24 01:03:18 +0200

    Created first Test of controller class

---
+++ src/test/java/com/example/kinobackend/controllers/CustomerControllerTest.java
@@ -0,0 +1,49 @@
+package com.example.kinobackend.controllers;
+
+import com.example.kinobackend.db_access.CustomerSQL;
+import com.example.kinobackend.responses.Customer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class CustomerControllerTest {
+
+    CustomerController testCustomerController;
+
+    @BeforeEach
+    void setUp() {
+        testCustomerController = new CustomerController();
+    }
+
+    @AfterEach
+    void tearDown() {
+        testCustomerController = null;
+    }
+
+    @Test
+    void getCustomers() {
+        Customer[] actualCustomers = testCustomerController.getCustomers();
+
+        CustomerSQL testCustomerSQL = new CustomerSQL();
+        Customer[] expectedCustomers = testCustomerSQL.getCustomerData();
+
+        assertThat(expectedCustomers).usingRecursiveComparison().ignoringFields("birthDate").isEqualTo(actualCustomers);
+    }
+
+    @Test
+    void getCustomerByMailAdress() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+}
