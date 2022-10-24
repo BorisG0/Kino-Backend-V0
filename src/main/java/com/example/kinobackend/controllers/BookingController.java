23992f344673e083da1830e53e9806b13c7d6fd1
Boris Gratchev <boris@gratchev.com>
2022-10-24 10:57:06 +0200

    new db script

--- src/main/java/com/example/kinobackend/controllers/BookingController.java
+++ src/main/java/com/example/kinobackend/controllers/BookingController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Book;
+
 @RestController
 public class BookingController {
     @PostMapping("/api/newBooking")
@@ -17,8 +19,10 @@ public class BookingController {
         System.out.println("new booking creation:  customer=" + bookingCreation.getEmail() + " tickets=" + bookingCreation.getTicketIds().length);
 
         SeatSQL seatSQL = new SeatSQL();
+        BookingSQL bookingSQL = new BookingSQL();
         for(int id: bookingCreation.getTicketIds()){
             seatSQL.setSeatInEventStatus(new SeatInEvent("", 1, 1, id, 3));
+            bookingSQL.setStatusForTicket(new StatusChange(id, 1));
         }
 
         return true;
