f936a7d7a3966fef593ef4d4c125494712afff82
Mel Wieczorek <mel.wieczorek@gmx.de>
2022-10-24 19:47:05 +0200

    Added empMovies

--- src/main/java/com/example/kinobackend/db_access/MovieSQL.java
+++ src/main/java/com/example/kinobackend/db_access/MovieSQL.java
@@ -29,6 +29,26 @@ public final class MovieSQL extends MySqlConnector{
         return data.toArray(new Movie[0]);
     }
 
+    public Movie[] getMovieDataForEmployee(){
+        ArrayList<Movie> data = new ArrayList<>();
+
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from movie");
+
+            while(rs.next()){
+                data.add(new Movie(rs.getInt(1), rs.getString(2), rs.getInt(3),
+                        rs.getInt(4), rs.getString(5), rs.getString(6),
+                        rs.getString(7), rs.getDate(8), rs.getString(9),
+                        rs.getString(10), rs.getString(11), rs.getString(12)));
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return data.toArray(new Movie[0]);
+    }
+
     public Movie getMovieById(int id){
         Movie movie = null;
 
