bbc5fed6aec2cf69f08aa549e06776a4e9476000
Mel Wieczorek <mel.wieczorek@gmx.de>
2022-10-28 10:35:05 +0200

    Corrected all foreignKey mistakes, added check for existing events in updateEvent

--- src/main/java/com/example/kinobackend/db_access/BookingSQL.java
+++ src/main/java/com/example/kinobackend/db_access/BookingSQL.java
@@ -28,7 +28,7 @@ public class BookingSQL extends MySqlConnector{
         int id = 0;
         try {
             Statement stmt = con.createStatement();
-            stmt.execute("INSERT INTO booking (email, pricePaid) VALUES ('" + email +"', " + pricePaid + ");");
+            stmt.execute("INSERT INTO booking (customer_email, pricePaid) VALUES ('" + email +"', " + pricePaid + ");");
 
             stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select idBooking from booking order by idBooking desc limit 1");
@@ -46,7 +46,7 @@ public class BookingSQL extends MySqlConnector{
         try{
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select t.idTicket, s.Line, s.NumberInLine, t.status, " +
-                    "t.defaultPrice from ticket t, seat s where t.idSeat = s.idSeat and idEvent = " + id);
+                    "t.defaultPrice from ticket t, seat s where t.seat_idSeat = s.idSeat and event_idEvent = " + id);
 
             while(rs.next()){
                 Ticket t = new Ticket(rs.getInt(1), rs.getString(2), rs.getInt(3),
@@ -65,15 +65,15 @@ public class BookingSQL extends MySqlConnector{
 
         try{
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from booking where email = '" + email + "'");
+            ResultSet rs = stmt.executeQuery("select * from booking where customer_email = " + putStringIntoApostrophe(email));
 
             while(rs.next()){
 
 
                 Statement stmt2 = con.createStatement();
-                ResultSet rs2 = stmt2.executeQuery("select s.Line, s.NumberInLine, t.defaultPrice, m.title, e.Date, e.Time from ticket t, seat s, movie m, `event` e " +
-                        "where s.idSeat = t.idSeat and t.idEvent = e.idEvent and e.movie_idMovie = m.idMovie " +
-                        "and idBooking = " + rs.getInt(1));
+                ResultSet rs2 = stmt2.executeQuery("select s.Line, s.NumberInLine, t.defaultPrice, m.title, e.Date, e.Time from ticket t, seat s, movie m, event e " +
+                        "where s.idSeat = t.seat_idSeat and t.event_idEvent = e.idEvent and e.movie_idMovie = m.idMovie " +
+                        "and booking_idBooking = " + rs.getInt(1));
 
                 rs2.next();
 
@@ -105,7 +105,7 @@ public class BookingSQL extends MySqlConnector{
                 stmt.executeUpdate("UPDATE ticket SET status = " + statusChange.getStatus() + " WHERE (idTicket = " + statusChange.getId() + ")");
             }else{
                 stmt.executeUpdate("UPDATE ticket SET status = " + statusChange.getStatus()
-                        +", idBooking = " + bookingId + " WHERE (idTicket = " + statusChange.getId() + ")");
+                        +", booking_idBooking = " + bookingId + " WHERE (idTicket = " + statusChange.getId() + ")");
             }
 
 
