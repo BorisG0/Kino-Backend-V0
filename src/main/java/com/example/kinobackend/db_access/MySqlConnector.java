c05c4a58a76400d48a9a9b4b8b2ca54d238406b0
ds704 <42025411+ds704@users.noreply.github.com>
2022-10-30 01:10:00 +0200

    Update for Jenkins

--- src/main/java/com/example/kinobackend/db_access/MySqlConnector.java
+++ src/main/java/com/example/kinobackend/db_access/MySqlConnector.java
@@ -14,6 +14,7 @@ public class MySqlConnector {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "DBADMIN");
+            System.out.println("Test für Jenkins");
         }catch (Exception e){
             System.out.println(e);
         }
